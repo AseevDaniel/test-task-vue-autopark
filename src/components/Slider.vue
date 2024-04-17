7bfe616e9cb9f518f56163194304cd06530861d6
Asieiev Danylo <danylo.asieiev@nixsolutions.com>
2024-04-17 20:18:14 +0300

    added slider custom elements

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <v-carousel v-model="currentImage">
+    <v-carousel hide-delimiter-background v-model="currentImage">
+      <template v-slot:prev="{ props }">
+        <div class="arrowWrapper">
+          <ArrowIcon class="arrowIcon prevIcon" @click="props.onClick" />
+        </div>
+      </template>
+      <template v-slot:next="{ props }">
+        <div class="arrowWrapper">
+          <ArrowIcon class="arrowIcon nextIcon" @click="props.onClick" />
+        </div>
+      </template>
       <v-carousel-item v-for="(image, i) in images" :key="image" :value="i">
         <div class="d-flex fill-height justify-center align-center">
           <img class="fill-height fill-width" :src="image" :alt="'Auto photo' + (i + 1)" />
@@ -11,7 +21,11 @@
 </template>
 
 <script>
+import ArrowIcon from './icons/IconArrowDown.vue'
 export default {
+  components: {
+    ArrowIcon
+  },
   data() {
     return {
       currentImage: 0
@@ -24,4 +38,61 @@ export default {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+.v-carousel {
+  .arrowWrapper {
+    height: 24px;
+    width: 24px;
+    background: transparent;
+    backdrop-filter: blur(4px);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: 0.3s;
+    &:hover {
+      scale: 1.1;
+    }
+    .arrowIcon {
+      position: relative;
+      height: 18px;
+      path {
+        stroke: white;
+      }
+      &.prevIcon {
+        rotate: 90deg;
+      }
+      &.nextIcon {
+        rotate: 270deg;
+      }
+    }
+  }
+
+  .v-carousel__controls {
+    height: 24px !important;
+    .v-carousel__controls__item.v-btn.v-btn--icon {
+      background-color: $mainColor;
+      opacity: 0.4;
+      height: 8px;
+      width: 8px;
+      margin: 0 2px;
+      &.v-btn--active,
+      &:hover,
+      &:focus {
+        opacity: 1;
+      }
+    }
+
+    .v-btn__content .v-icon {
+      display: none; /* Removes the default icon */
+    }
+    .v-btn--variant-elevated,
+    .v-btn--variant-flat {
+      background: transparent;
+      color: white;
+      backdrop-filter: blur(4px);
+    }
+  }
+}
+</style>
