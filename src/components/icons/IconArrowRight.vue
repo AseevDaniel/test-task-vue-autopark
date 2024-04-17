0c636746981f8b27fa4e96344ac43eab25e0181d
Asieiev Danylo <danylo.asieiev@nixsolutions.com>
2024-04-17 14:17:39 +0300

    added icons

---
+++ src/components/icons/IconArrowRight.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4.7207 11.75L19.7207 11.75M19.7207 11.75L12.9707 18.5M19.7207 11.75L12.9707 5"
+      stroke="#161616"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/components/icons/IconEyeClose.vue
@@ -0,0 +1,66 @@
+<template>
+  <svg width="32" height="29" viewBox="0 0 32 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_0_783)">
+      <path
+        d="M8.5 3.75L23.5 20.25"
+        stroke="#161616"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M18.5228 14.775C17.7868 15.4441 16.8152 15.7934 15.8217 15.7461C14.8281 15.6988 13.8941 15.2588 13.225 14.5228C12.5559 13.7868 12.2066 12.8152 12.2539 11.8217C12.3012 10.8281 12.7412 9.89407 13.4772 9.22498"
+        stroke="#161616"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M16.7059 8.31653C17.5032 8.4692 18.2294 8.87644 18.7754 9.47708C19.3214 10.0777 19.6578 10.8393 19.7341 11.6475"
+        stroke="#161616"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.5572 15.8531C25.6009 14.0231 26.5 12 26.5 12C26.5 12 23.5 5.25001 16 5.25001C15.3505 5.24912 14.7021 5.3018 14.0612 5.40751"
+        stroke="#161616"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.9375 6.43127C7.11531 8.36627 5.5 12 5.5 12C5.5 12 8.5 18.75 16 18.75C17.7574 18.7638 19.4927 18.3589 21.0625 17.5688"
+        stroke="#161616"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_0_783"
+        x="0"
+        y="0"
+        width="32"
+        height="32"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_0_783" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_0_783" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+</template>
